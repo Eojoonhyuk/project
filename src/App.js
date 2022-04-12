2a7a36e40ae805f54c064318448575fc0b6463e4
Eojoonhyuk <anftlcb@gmail.com>
2022-04-12 15:05:16 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {useState} from 'react';
+
+function Header(props){
+  return(
+    <div className="header">
+      <div className="header_logo">
+        <h1><a href="/">{props.title}</a></h1>
+      </div>
+      <div className="search">
+        <form>
+              <input type="text" className="search_input" placeholder="검색어를 입력해주세요."></input>
+              <button type="submit" className="search_sumit">
+                <span className="blind">검색</span>
+              </button>
+        </form>         
+    </div>
+    <div className="user">
+      <ul>
+        <il><a href="/">로그인</a></il>
+      </ul>
+    </div>
+
+    </div>
+  );
+}
+
+function Container(){
+  return(
+  <div className="main_container">
+    <div className="aside">
+      <div className="test">
+          <ul><li><a href="/">테스트</a></li></ul>
+      </div>
+      <div className="catagory">
+        <ul>
+          <li><a href="/">menu1</a></li>
+          <li><a href="/">menu2</a></li>
+          <li><a href="/">menu3</a></li>
+          <li><a href="/">menu4</a></li>
+          <li><a href="/">menu5</a></li>
+      </ul>
+      </div>
+    </div>
+    <div className="section">
+      <h2>display</h2>
+    </div>
+  </div>
+  );
+}
+
+function Footer(){
+  return(
+  <div className="footer">
+    <h2>footer</h2>
+  </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="(로고)"></Header>
+      <Container></Container>
+      <Footer></Footer>
     </div>
   );
 }
